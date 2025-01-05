0503e998a5baa157e0f7a17622f929e7b0b7d3d5
Parth7729 <guptaparth716@gmail.com>
2025-01-05 13:19:49 +0530

    created tables with indexes

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,9 +1,49 @@
 # --- !Ups
 
-create table counts(
-id BIGSERIAL PRIMARY key
+CREATE TABLE IF NOT EXISTS restaurants(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ,
+    status TEXT NOT NULL,
+    last_contacted_at TIMESTAMPTZ,
+    max_call_per_week INTEGER
 );
+
+
+CREATE TABLE IF NOT EXISTS points_of_contact(
+    id BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
+    phone VARCHAR(255),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ,
+    role TEXT NOT NULL,
+    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
+    last_contacted_at TIMESTAMPTZ
+);
+
+
+CREATE TABLE IF NOT EXISTS interactions(
+
+    id BIGSERIAL PRIMARY KEY,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    interaction_type TEXT NOT NULL,
+    poc_id BIGINT NOT NULL REFERENCES points_of_contact(id),
+    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
+    call_recording_url VARCHAR(255)
+
+);
+
+CREATE INDEX IF NOT EXISTS lms_poc_restaurant_id ON points_of_contact(restaurant_id);
+
+CREATE INDEX IF NOT EXISTS lms_interactions_poc_id_restaurant_id ON interactions(poc_id, restaurant_id);
 
 # --- !Downs
 
-drop table counts;
+DROP TABLE IF EXISTS interactions;
+
+DROP TABLE IF EXISTS points_of_contact;
+
+DROP TABLE IF EXISTS restaurants;
